c72b723612cba386a1891814a059996f0506c405
pablo-corrales <charliequiro>
2014-10-07 21:27:16 +0200

    Commit 4 develop

--- src/main/java/ecp1/pablo/corrales/HolaMundo.java
+++ src/main/java/ecp1/pablo/corrales/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!"); 
         System.out.println("Pablo Corrales"); 
-        System.out.println("ECP1"); 
+        System.out.println("ECP1-commit 4"); 
                
     }
 
